6a269cc37e85ceecca08e528f0ffe33caa6e5382
Jhace <tdjhace.lesanglierbarotcher@gmail.com>
2020-11-06 19:32:32 +0100

    correctif sur l'interface du main satellite

--- src/SatConception/SatMain.java
+++ src/SatConception/SatMain.java
@@ -10,25 +10,17 @@ public class SatMain {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome in the Satellite Main Program.");
         System.out.println("You basically have nothing to do.");
-        System.out.println("Just write below EXIT when your job is done.");
+        System.out.println("Just press Crtl + C when your job is done.");
 
-        Scanner sc = new Scanner(System.in);
         File referenceFile = new File("src/CHANNELS/");
-        String[] listSat = referenceFile.list();
-        // listSat contains all the directory names in CHANNELS
+        String[] listSat = referenceFile.list(); // listSat contains all the directory names in CHANNELS
 
         while (true) {
-            // if (sc.hasNext()) {
-            // System.out.println("yes");
-            // if (sc.next().equals("EXIT")) {
-            // sc.close();
-            // break;
-            // }
-            // }
 
             for (String satName : listSat) {
                 String satDir = "src/CHANNELS/" + satName + "/UPLINK";
                 File testFile = new File(satDir);
+                System.out.println("prout");
                 if (testFile.canRead()) {
                     FileReader in = new FileReader(satDir);
                     BufferedReader bin = new BufferedReader(in);
@@ -37,6 +29,7 @@ public class SatMain {
                     testFile.delete();
                 }
             }
+
         }
 
     }
